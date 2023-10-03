c3f0ad5d87682cbf929a4e6092001e26e485cbfc
alezfi96 <alezfigueroa@hotmail.com>
2023-10-03 00:56:53 -0500

    apidewasap2

--- app.js
+++ app.js
@@ -3,13 +3,29 @@ const apiruta = require('./src/routes/ruta');
 
 const app = express();
 
-const PORT = process.env.PORT || 8080
-;
+// const PORT = process.env.PORT || 8080
+
+const PORT = 8080;
 
 app.use(express.json());
-app.use("/api",apiruta);
+
+// app.use("/api",apiruta);
+app.get('/api',apiruta, (req, res) => {
+    // res.send('Hola, mundo!');
+  });
+  
 
 
 app.listen(PORT,()=>{
-    console.log("El puerto de la API es : ", PORT);
+    console.log(`El servidor está escuchando en el puerto ${PORT}`);
 })
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hola, mundo!');
+});
+
